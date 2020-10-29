941ffc413917c759edb9765bc7306e08ccabcdd8
marlenaJakubowska <mar.jakubowska90@gmail.com>
2020-10-29 12:01:32 +0100

    Add skeleton css

--- src/main/java/com/codecool/loginPage/App.java
+++ src/main/java/com/codecool/loginPage/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
         LocalDB localDB = new LocalDB();
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8001), 0);
         server.createContext("/login", new LogIn(localDB));
         server.createContext("/logout", new LogOut(localDB));
         server.createContext("/static", new Static());
